3dfde513cbe2ad358f50485fa929a2caf4443c27
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2022-02-14 18:49:47 +0300

    small changes to css

--- client/src/GameReviews.js
+++ client/src/GameReviews.js
@@ -9,7 +9,7 @@ function GameReviews({ gameReviews }) {
     return (
         <div>
             {gameReviews.reviews?.map((review) => (<div className="review" key={review.id}><p>Title: {review.title}</p>
-                <p>Rating: {review.rating}</p>
+                <p>Rating: {review.rating}/5</p>
                 <p>Description: {review.description}</p>
             </div>))}
         </div>
